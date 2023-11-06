6a729974da0e6a045bd47f1a1c367ee07fb55180
André Rebelo <andrecsrebelo@gmail.com>
2023-11-06 10:58:27 +0000

    Microservice beer
    1

---
+++ src/main/java/onlymr/microservicebeer/MicroServiceBeerApplication.java
@@ -0,0 +1,13 @@
+package onlymr.microservicebeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceBeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/onlymr/microservicebeer/MicroServiceBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package onlymr.microservicebeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceBeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
