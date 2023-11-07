c4d1db6d6f9ecf589f40535c8f5714216b94e724
André Rebelo <andrecsrebelo@gmail.com>
2023-11-07 11:52:04 +0000

    adding Beer Rep

---
+++ src/main/java/Repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package Repositories;
+
+import onlymr.microservicebeer.client.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+
+}
--- src/main/java/onlymr/microservicebeer/client/domain/Beer.java
+++ src/main/java/onlymr/microservicebeer/client/domain/Beer.java
@@ -1,4 +1,4 @@
-package domain;
+package onlymr.microservicebeer.client.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
