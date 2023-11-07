522b3a6a22323121e5fca5ec01618bcf6897717f
André Rebelo <andrecsrebelo@gmail.com>
2023-11-07 11:56:00 +0000

    adding Beer Rep

---
+++ src/main/java/onlymr/microservicebeer/client/bootStrap/BeerLoader.java
@@ -0,0 +1,9 @@
+package onlymr.microservicebeer.client.bootStrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+}
